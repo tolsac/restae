dea97233e0ac41a4f6b9ae941d0356258a123136
camilletolsa <camille.tolsa@innovela.com>
2018-07-31 17:12:48 +0200

    add docs

--- restae/__init__.py
+++ restae/__init__.py
@@ -3,4 +3,4 @@ import webapp2
 # Fix to allow the use of PATCH command with webapp2 base framework
 webapp2.WSGIApplication.allowed_methods = webapp2.WSGIApplication.allowed_methods.union(('PATCH',))
 
-__version__ = '1.0'
+__version__ = '1.1'
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author_email="camille.tolsa@gmail.com",
     description="REST Framwork for webapp2 & datastore applications",
     long_description=open('README.md').read(),
-    url='http://github.com/tolsac/gae-rest-framework',
+    url='http://github.com/tolsac/restae',
     include_package_data=True,
     install_requires=[],
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
@@ -28,10 +28,6 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
     ]
 
