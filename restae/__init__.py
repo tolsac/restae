8fe5e701d542b2da5a67f70127f84a50393fbc99
camilletolsa <camille.tolsa@innovela.com>
2018-08-08 13:12:29 +0200

    bump version

--- restae/__init__.py
+++ restae/__init__.py
@@ -3,4 +3,4 @@ import webapp2
 # Fix to allow the use of PATCH command with webapp2 base framework
 webapp2.WSGIApplication.allowed_methods = webapp2.WSGIApplication.allowed_methods.union(('PATCH',))
 
-__version__ = '1.1.1'
+__version__ = '1.1.2'
